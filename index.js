b1caa675c070e69a39a5f16398cc6db00a93dd45
Lepha <lepharamchiary@gmail.com>
2024-08-06 15:33:14 +0530

    add json data for testing

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
+const githubData = {
+    "login": "kevwil",
+    "id": 35,
+    "node_id": "MDQ6VXNlcjM1",
+    "avatar_url": "https://avatars.githubusercontent.com/u/35?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/kevwil",
+    "html_url": "https://github.com/kevwil",
+    "followers_url": "https://api.github.com/users/kevwil/followers",
+    "following_url": "https://api.github.com/users/kevwil/following{/other_user}",
+    "gists_url": "https://api.github.com/users/kevwil/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/kevwil/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/kevwil/subscriptions",
+    "organizations_url": "https://api.github.com/users/kevwil/orgs",
+    "repos_url": "https://api.github.com/users/kevwil/repos",
+    "events_url": "https://api.github.com/users/kevwil/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/kevwil/received_events",
+    "type": "User",
+    "site_admin": false
+}
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
@@ -14,6 +35,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.send("<h1>Pls login</h1>")
+})
+
+app.get("/github", (req, res) => {
+    res.json(githubData);
 })
 
 app.listen(port, () => {
